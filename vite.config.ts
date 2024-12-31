0ba21820450771a9ac3cf5d2a7b5d9987078109a
이태관 <>
2024-12-31 17:51:39 +0900

    Refactor: emotion autoLabel 기능 활성화

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import path from 'path';
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: '@emotion/react'
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
     })
   ],
   resolve: {
