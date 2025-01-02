d275a0479ce757f14301bebb701ded55de080d94
이태관 <cokid98@naver.com>
2025-01-02 15:10:24 +0900

    Refactor: globalStyle file 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { Global } from '@emotion/react';
-import GlobalStyle from '@/Style/GlobalStyle';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/Style/theme';
 import App from './App';
+import resetCss from 'emotion-reset';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,8 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <ThemeProvider theme={theme}>
-    <div css={(theme) => ({ color: theme.colors.cream })}>asdf</div>
-    <Global styles={GlobalStyle} />
+    <Global styles={resetCss} />
     <App />
   </ThemeProvider>
 );
